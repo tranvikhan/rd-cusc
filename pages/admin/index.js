392dabc861069de6082c1145e5d8cde746fa9ec7
tranvikhan <tranvikhan@gmail.com>
2021-05-08 22:37:08 +0700

    Fix Bug affter have dinner

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,7 +1,9 @@
 import dynamic from 'next/dynamic'
 import WebHead from '../../components/Layout/head'
 import Header from '../../components/Layout/header'
-const BasicDemo = dynamic(() => import('../../components/Editor'))
+const BasicDemo = dynamic(() => import('../../components/Editor'), {
+  ssr: false,
+})
 export default function index() {
   return (
     <article>
